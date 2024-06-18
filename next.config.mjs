b4d6c7869339d6cd331801d4d6bd5253196a5fbb
michellechung099 <mchung099@gmail.com>
2024-06-18 15:25:23 -0700

    fix nextjs image configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["github-readme-stats.vercel.app", "skillicons.dev"],
+  },
+  env: {
+    PUBLIC_GITHUB_STATS_URL: process.env.PUBLIC_GITHUB_STATS_URL,
+  },
+};
 
 export default nextConfig;
