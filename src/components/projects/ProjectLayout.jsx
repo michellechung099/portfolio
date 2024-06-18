026ef2b35e3d6fe62ef6277c5f69d994b73846a3
michellechung099 <mchung099@gmail.com>
2024-06-18 14:34:35 -0700

    add mobile responsiveness to projects page

--- src/components/projects/ProjectLayout.jsx
+++ src/components/projects/ProjectLayout.jsx
@@ -4,25 +4,27 @@ import React from "react";
 
 const ProjectLayout = ({ name, description, demoLink, sourceCode }) => {
   return (
-    <Link
-      href={demoLink}
-      target={"_blank"}
-      className="flex items-center justify-between w-full relative rounded-lg overflow-hidden p-6 custom-bg"
-    >
-      <div className="flex items-center justify-center space-x-2">
-        <h2 className="text-foreground text-sm">{name} - </h2>
-        <p className="text-foreground text-sm">{description}</p>
-      </div>
-      {/* <div className="self-end flex-1 mx-2 mb-1 bg-transparent border-b border-dashed border-foreground" /> */}
-      <a
+    <div className="text-sm md:text-base flex items-center justify-between w-full relative rounded-lg overflow-hidden p-4 md:p-6 custom-bg">
+      <Link
+        href={demoLink}
+        target={"_blank"}
+        className="flex-grow flex items-center justify-start space-x-2"
+      >
+        <div className="flex items-center justify-center space-x-2">
+          <h2 className="text-foreground text-sm">{name}</h2>
+          <p className="text-muted text-sm">{description}</p>
+        </div>
+      </Link>
+
+      <Link
         href={sourceCode}
         className="text-foreground"
         target="_blank"
         rel="noopener noreferrer"
       >
-        <Github className="w-full h-auto" strokeWidth={1.5} />
-      </a>
-    </Link>
+        <Github className="w-5 h-5" strokeWidth={1.5} />
+      </Link>
+    </div>
   );
 };
 
