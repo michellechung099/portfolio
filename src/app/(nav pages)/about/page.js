5a2b08d649b7ff47836e7e3b6a92d991a91f40d2
michellechung099 <mchung099@gmail.com>
2024-06-18 12:39:16 -0700

    about page mushroom model and name responsiveness

--- src/app/(nav pages)/about/page.js
+++ src/app/(nav pages)/about/page.js
@@ -12,15 +12,17 @@ export default function About() {
         alt="background-image"
         className="-z-50 fixed w-full top-0 left-0 h-full object-cover object-center opacity-80"
       />
-      <div className="w-full h-screen absolute -translate-x-9 -translate-y-[40%]">
+      <div className="w-full h-3/5 xs:h-[81%] sm:h-screen absolute top-3/4 sm:top-[80%] xs:top-[78%] sm:-left-[4%] xs:-left-[5%] -translate-y-1/2 -left-1/6">
         <RenderModel>
           <Mushroom />
         </RenderModel>
       </div>
 
       <div className="relative w-full h-screen flex flex-col items-center justify-center">
-        <div className="absolute flex flex-col items-center text-center top-[4%] left-1/2 -translate-x-1/2">
-          <h1 className="font-bold text-9xl text-accent">Michelle</h1>
+        <div className="absolute flex flex-col items-center text-center top-[20%] sm:top-[15%] left-1/2 -translate-y-1/2 -translate-x-1/2">
+          <h1 className="font-bold text-7xl xs:text-8xl sm:text-9xl lg:text-9xl text-accent">
+            Michelle
+          </h1>
           <p className="font-light text-foreground text-ls">
             software engineer based in San Francisco
           </p>
