f1157acbaaafc6ec8329549c9d896dcc90d3cd9a
michellechung099 <mchung099@gmail.com>
2024-06-21 17:41:48 -0700

    order projects by most recent project worked on

--- src/components/projects/index.jsx
+++ src/components/projects/index.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import ProjectLayout from "./ProjectLayout";
+import { projectData } from "@/app/portfolioData";
+
+const reverseList = (projects) => {
+  // create a shallow copy of projects to not mutate them in-place
+  // O(n log n) time
+  return [...projects].sort((a, b) => b.id - a.id);
+};
 
 const ProjectList = ({ projects }) => {
   return (
     <div className="w-full max-w-auto xl:max-w-4xl px-4 mx-auto lg:px-10 space-y-6 md:space-y-8 sm:space-y-9 flex flex-col items-center">
-      {projects.map((project, index) => {
+      {reverseList(projects).map((project, index) => {
         return <ProjectLayout key={index} {...project} />;
       })}
     </div>
